60ad35b2b04639544efbf900b5bb2d8e05d482fc
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-03-26 22:29:06 +0530

    Initial Commit

---
+++ RPSGame/RPSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameApp.swift
+//  RPSGame
+//
+//  Created by Hitesh Madaan on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RPSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
