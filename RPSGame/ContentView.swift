345dd91391160e272996a09f5b4d398c1d2b2bc4
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-04-06 12:34:29 +0530

    Added the Images

--- RPSGame/ContentView.swift
+++ RPSGame/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var elements=["Rock","Paper","Scissor"]
-    @State private var selected="Rock"
+    var elements=["Rock","Paper","Scissors"]
+    @State private var selected=""
     @State private var random=1
+    @State private var random2=1
+
     @State private var computed=""
    
     
@@ -20,9 +22,14 @@ struct ContentView: View {
         selected=e
         
     }
+    
+    func select(_ n:Int){
+        random2=n
+        check()
+    }
     func check(){
+        
         random=Int.random(in: 0..<3)
-        let random2=Int.random(in: 0..<3)
         computed=elements[random]
         selected=elements[random2]
         
@@ -33,11 +40,11 @@ struct ContentView: View {
         {
             print("Loose")
         }
-        else if(selected=="Rock"&&elements[random]=="Scissor")
+        else if(selected=="Rock"&&elements[random]=="Scissors")
         {
             print("Win")
         }
-        else if(selected=="Paper"&&elements[random]=="Scissor")
+        else if(selected=="Paper"&&elements[random]=="Scissors")
         {
             print("Loose")
         }
@@ -45,11 +52,11 @@ struct ContentView: View {
         {
             print("Win")
         }
-        else if(selected=="Scissor"&&elements[random]=="Rock")
+        else if(selected=="Scissors"&&elements[random]=="Rock")
         {
             print("Loose")
         }
-        else if(selected=="Scissor"&&elements[random]=="Paper")
+        else if(selected=="Scissors"&&elements[random]=="Paper")
         {
             print("Win")
         }
@@ -67,36 +74,67 @@ struct ContentView: View {
                 VStack{
                     
                     Text("Select Your Element")
+                        .font(.largeTitle)
                     
                     
-                    Picker("Select the Elemets",selection: $selected){
-                        ForEach(elements,id: \.self){ e in
-                            
-                            Text(e)
-                            
+//                    Picker("Select the Elemets",selection: $selected){
+//                        ForEach(elements,id: \.self){ e in
+//                            
+//                            Text(e)
+//                            
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                    .padding(40)
+                    
+                    HStack{
+                        ForEach(0..<3){n in
+                            Button{
+                                
+                               select(n)
+                                
+                            }label: {
+                                Image(elements[n])
+                                    .resizable()
+                                    .frame(width: 100,height: 150)
+                            }
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .frame(maxWidth: 450,maxHeight: 450)
                     .padding(40)
                     
+                    Spacer()
                     
-                    Picker("Computer element",selection: $computed){
-                        ForEach(elements,id: \.self){
-                            Text($0)
-                        }
+                    Text("Results")
+                    HStack{
+                        Image(elements[random2])
+                            .resizable()
+                            .frame(width: 100,height: 100)
+                        
+                        Text("Vs")
+                        
+                        Image(elements[random])
+                            .resizable()
+                            .frame(width: 100,height: 100)
+                        
                     }
-                    .pickerStyle(.segmented)
-                    .padding(40)
+//                    Picker("Computer element",selection: $computed){
+//                        ForEach(elements,id: \.self){
+//                            Text($0)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                    .padding(40)
                     
                     
                     Spacer()
                 }
                 Spacer()
-                Button("Click"){
-                    check()
-                }
+                
             }
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: 400)
+            .frame(maxWidth: 350,maxHeight: 500)
+            
+            
             
             .navigationTitle("Rock Paper Scissors")
             
